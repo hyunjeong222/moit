f7defee58f6479206f4de748bdab6da307723b24
hyunjeong222 <hyenj629@naver.com>
2024-11-13 17:46:38 +0900

    init

---
+++ src/main/java/com/sping/moit/MoitApplication.java
@@ -0,0 +1,11 @@
+package com.sping.moit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoitApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MoitApplication.class, args);
+	}
+}
